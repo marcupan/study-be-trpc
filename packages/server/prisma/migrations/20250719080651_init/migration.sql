18efff1d8dc2f92a8c8a8b3b20eb5e5cb16be4f7
meloy <10772794+marcupan@users.noreply.github.com>
2025-10-31 18:20:13 +0200

    update prisma

--- packages/server/prisma/migrations/20250719080651_init/migration.sql
+++ packages/server/prisma/migrations/20250719080651_init/migration.sql
@@ -1,36 +1,39 @@
 -- CreateTable
-CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
-    "name" TEXT,
+CREATE TABLE "User"
+(
+    "id"        TEXT     NOT NULL PRIMARY KEY,
+    "email"     TEXT     NOT NULL,
+    "password"  TEXT     NOT NULL,
+    "name"      TEXT,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
 
 -- CreateTable
-CREATE TABLE "Board" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
+CREATE TABLE "Board"
+(
+    "id"          TEXT     NOT NULL PRIMARY KEY,
+    "name"        TEXT     NOT NULL,
     "description" TEXT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    "ownerId" TEXT NOT NULL,
+    "createdAt"   DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt"   DATETIME NOT NULL,
+    "ownerId"     TEXT     NOT NULL,
     CONSTRAINT "Board_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateTable
-CREATE TABLE "Task" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
+CREATE TABLE "Task"
+(
+    "id"          TEXT     NOT NULL PRIMARY KEY,
+    "title"       TEXT     NOT NULL,
     "description" TEXT,
-    "status" TEXT NOT NULL DEFAULT 'Todo',
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    "boardId" TEXT NOT NULL,
-    "assigneeId" TEXT,
-    "creatorId" TEXT NOT NULL,
-    "updaterId" TEXT NOT NULL,
+    "status"      TEXT     NOT NULL DEFAULT 'Todo',
+    "createdAt"   DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt"   DATETIME NOT NULL,
+    "boardId"     TEXT     NOT NULL,
+    "assigneeId"  TEXT,
+    "creatorId"   TEXT     NOT NULL,
+    "updaterId"   TEXT     NOT NULL,
     CONSTRAINT "Task_boardId_fkey" FOREIGN KEY ("boardId") REFERENCES "Board" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT "Task_assigneeId_fkey" FOREIGN KEY ("assigneeId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
     CONSTRAINT "Task_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
@@ -38,19 +41,20 @@ CREATE TABLE "Task" (
 );
 
 -- CreateTable
-CREATE TABLE "Collaboration" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "accessLevel" TEXT NOT NULL DEFAULT 'read',
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    "userId" TEXT NOT NULL,
-    "boardId" TEXT NOT NULL,
+CREATE TABLE "Collaboration"
+(
+    "id"          TEXT     NOT NULL PRIMARY KEY,
+    "accessLevel" TEXT     NOT NULL DEFAULT 'read',
+    "createdAt"   DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt"   DATETIME NOT NULL,
+    "userId"      TEXT     NOT NULL,
+    "boardId"     TEXT     NOT NULL,
     CONSTRAINT "Collaboration_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT "Collaboration_boardId_fkey" FOREIGN KEY ("boardId") REFERENCES "Board" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "User_email_key" ON "User" ("email");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Collaboration_userId_boardId_key" ON "Collaboration"("userId", "boardId");
+CREATE UNIQUE INDEX "Collaboration_userId_boardId_key" ON "Collaboration" ("userId", "boardId");
