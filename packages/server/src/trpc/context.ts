8e728fa170d739c4a3541c313ca430889daefc12
meloy <10772794+marcupan@users.noreply.github.com>
2025-11-10 21:31:36 +0200

    update trpc

--- packages/server/src/trpc/context.ts
+++ packages/server/src/trpc/context.ts
@@ -1,7 +1,7 @@
-import { inferAsyncReturnType } from '@trpc/server';
-import { CreateExpressContextOptions } from '@trpc/server/adapters/express';
-import { PrismaClient } from '@prisma/client';
-import { verifyToken } from '../utils/auth';
+import {inferAsyncReturnType} from '@trpc/server';
+import {CreateExpressContextOptions} from '@trpc/server/adapters/express';
+import {PrismaClient} from '@prisma/client';
+import {verifyToken} from '../utils/auth';
 
 // Create a single instance of the Prisma client
 const prisma = new PrismaClient();
@@ -9,19 +9,19 @@ const prisma = new PrismaClient();
 /**
  * Creates context for the tRPC server
  */
-export const createContext = async ({ req, res }: CreateExpressContextOptions) => {
-  // Get the user token from the headers
-  const token = req.headers.authorization?.split(' ')[1];
+export const createContext = async ({req, res}: CreateExpressContextOptions) => {
+    // Get the user token from the headers
+    const token = req.headers.authorization?.split(' ')[1];
 
-  // Try to retrieve a user with the token
-  const user = token ? await verifyToken(token) : null;
+    // Try to retrieve a user with the token
+    const user = token ? await verifyToken(token) : null;
 
-  return {
-    prisma,
-    req,
-    res,
-    user,
-  };
+    return {
+        prisma,
+        req,
+        res,
+        user,
+    };
 };
 
 export type Context = inferAsyncReturnType<typeof createContext>;
--- packages/server/src/trpc/trpc.ts
+++ packages/server/src/trpc/trpc.ts
@@ -1,5 +1,6 @@
-import { initTRPC, TRPCError } from '@trpc/server';
-import { Context } from './context';
+import {initTRPC, TRPCError} from '@trpc/server';
+
+import {Context} from './context';
 
 // Initialize tRPC
 const t = initTRPC.context<Context>().create();
@@ -9,20 +10,20 @@ export const router = t.router;
 export const publicProcedure = t.procedure;
 
 // Middleware to check if user is authenticated
-const isAuthenticated = t.middleware(({ ctx, next }) => {
-  if (!ctx.user) {
-    throw new TRPCError({
-      code: 'UNAUTHORIZED',
-      message: 'You must be logged in to access this resource',
+const isAuthenticated = t.middleware(({ctx, next}) => {
+    if (!ctx.user) {
+        throw new TRPCError({
+            code: 'UNAUTHORIZED',
+            message: 'You must be logged in to access this resource',
+        });
+    }
+    return next({
+        ctx: {
+            ...ctx,
+            // Add user to context
+            user: ctx.user,
+        },
     });
-  }
-  return next({
-    ctx: {
-      ...ctx,
-      // Add user to context
-      user: ctx.user,
-    },
-  });
 });
 
 // Protected procedure that requires authentication
